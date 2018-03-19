646b9cbd2a703cb97082f6db9ca27695973ae390
Jasmin Husadzic <jasmin.husadzic@gmail.com>
2018-03-19 17:37:44 +0100

    import bootstrap

--- challengetwo/src/components/string.vue
+++ challengetwo/src/components/string.vue
@@ -1,19 +1,34 @@
 <template>
-    <div v-bind:class="[col]">
-        <label>{{label}}</label>
-        <input type="text" v-bind:value='value'>
+<div class="row">
+    <div v-bind:class="[firstName.col]">
+        <label>{{firstName.label}}</label>
+        <input type="text" v-bind:value='firstName.value'>
     </div>
+    <div v-bind:class="[firstName.col]">
+        <label>{{lastName.label}}</label>
+        <input type="text" v-bind:value='lastName.value'>
+    </div>
+</div>
 </template>
 <script>
 export default {
   name:'string',
   data(){
       return{
-        "label": "First name",
-        "name": "first_name",
-        "value": null,
-        "component": "string",
-        "col": 6
+        firstName:{
+            "label": "First name",
+            "name": "first_name",
+            "value": null,
+            "component": "string",
+            "col": "col-md-6"
+        },
+        lastName:{
+            "label": "Last name",
+            "name": "last_name",
+            "value": null,
+            "component": "string",
+            "col": "col-md-6"
+        } 
     }
   }
 }
